787f833914a54c287bffd5483b6b44f8a0861d21
standy <mostovoyav@gmail.com>
2015-12-30 04:04:45 +0300

    More caret tests

--- test/caret.spec.js
+++ test/caret.spec.js
@@ -9,16 +9,31 @@ describe('caret', function() {
 		$input.appendTo('body');
 		input = $input.get(0);
 		caret = caretInit(input);
-	});
-
-	it('should say Hello to the World', function() {
-		expect(caret).toBeDefined();
-	});
-
-	it('should get equal to set', function() {
 		input.focus();
-		caret.set(5);
+	});
 
+	it('should be zero for a start', function() {
+		expect(caret.get()).toEqual({begin: 0, end: 0});
+	});
+
+	it('should work for one argument', function() {
+		caret.set(5);
 		expect(caret.get()).toEqual({begin: 5, end: 5});
 	});
+
+	it('should work for two arguments', function() {
+		caret.set(3, 7);
+		expect(caret.get()).toEqual({begin: 3, end: 7});
+	});
+
+	it('should set last position for larger numbers', function() {
+		caret.set(15);
+		expect(caret.get()).toEqual({begin: 10, end: 10});
+	});
+
+	it('should set zero position for negative numbers', function() {
+		caret.set(-5);
+		expect(caret.get()).toEqual({begin: 0, end: 0});
+	});
+
 });
